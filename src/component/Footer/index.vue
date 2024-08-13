d6c9cc882b8718101d9e4191f042cfa03e8cfef3
luiju <luijunpeng@outlook.com>
2024-08-13 13:34:33 +0800

    Fix: Foot info add link to github

--- src/component/Footer/index.vue
+++ src/component/Footer/index.vue
@@ -11,8 +11,8 @@ let hostname = window.location.hostname;
       <span><a href="https://beian.miit.gov.cn/" target="_blank">{{ settingsRef.global.icp }}</a></span>
     </p>
     <p>
-      <span>Powered by ESC</span>
-      <span>Theme Esc-theme-default</span>
+      <span>Powered by <a href="https://github.com/luijp/esc-server" target="_blank">ESC</a></span>
+      <span>Theme <a href="https://github.com/luijp/esc-theme-default" target="_blank">Esc-theme-default</a></span>
     </p>
 
 
